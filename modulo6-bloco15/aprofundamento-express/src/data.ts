6d54dba5f45e93a90f1c70def839896bb68a93ac
Karla Natany <karlanatany3@gmail.com>
2022-03-31 23:44:14 -0300

    exercício do 1 ao 5

---
+++ modulo6-bloco15/aprofundamento-express/src/data.ts
@@ -0,0 +1,96 @@
+
+export type Task = {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+
+
+// type Post = {
+//   id: number,
+//   title: string,
+//   body: string,
+//   userId: number,
+// }
+
+export const taskList : Task[] = [
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 25,
+    "title": "voluptas quo tenetur perspiciatis explicabo natus",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 26,
+    "title": "aliquam aut quasi",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 27,
+    "title": "veritatis pariatur delectus",
+    "completed": true
+  },
+  {
+    "userId": 2,
+    "id": 28,
+    "title": "nesciunt totam sit blanditiis sit",
+    "completed": false
+  }
+]
+// export const users : User[] = [
+//   {
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//   },
+//   {
+//     "id": 2,
+//     "name": "Ervin Howell",
+//     "username": "Antonette",
+//     "email": "Shanna@melissa.tv",
+//     "phone": "010-692-6593 x09125",
+//     "website": "anastasia.net",
+//   },
+//   {
+//     "id": 3,
+//     "name": "Clementine Bauch",
+//     "username": "Samantha",
+//     "email": "Nathan@yesenia.net",
+//     "phone": "1-463-123-4447",
+//     "website": "ramiro.info",
+//   }
+
+// ]
+
+//resposta 6: criei um array separado, pois dentro do objeto de user achei complexo, fazer a tipagem dentreo de tipagem
