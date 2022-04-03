6baf37d278b4bad99ebac20d7f8d6a1130c0a246
Karla Natany <karlanatany3@gmail.com>
2022-04-03 19:57:57 -0300

    validação cpf

--- modulo6-bloco15/projeto-ts-iwfs/src/index.ts
+++ modulo6-bloco15/projeto-ts-iwfs/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { customerList } from './data'
+import { Customer, customerList } from './data'
 
 import { AddressInfo } from "net";
 
@@ -8,12 +8,30 @@ const app = express();
 app.use(express.json());
 
 app.get('/customers', (req: Request, res: Response) => {
-  // const pegaSaldoPositivo = customerList.filter((customer) => {
-  //   return customer.balance > 0
-  // })
   res.send(customerList)
+})
 
+//Crie um endpoint  que utilize o método POST da entidade users que  será responsável por cadastrar um usuário em um array de usuários. 
+app.post("/customers", (req: Request, res: Response) => {
+  const body: Customer = req.body
 
+  for(let customer of customerList){
+    if(body.cpf === customer.cpf){
+      return res.status(400).send("cpf ja existe")
+    } 
+  }
+
+  const newCustomer: Customer = {
+    name: body.name,
+    cpf: body.cpf,
+    birthDate: body.birthDate,
+    balance: 0,
+    transactions: []
+  }
+
+  customerList.push(newCustomer)
+
+  res.send('okzão')
 })
 
 const server = app.listen(process.env.PORT || 3003, () => {
