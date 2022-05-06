38a4872d53b23d709ea4a024111064874fad7fa9
Karla Natany <karlanatany3@gmail.com>
2022-05-05 23:46:53 -0300

    readme

--- modulo6-bloco17/labecommerce-backend/src/endpoints/getUsers.ts
+++ modulo6-bloco17/labecommerce-backend/src/endpoints/getUsers.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express"
 import { connection } from "../data/connection"
-import { User } from "../types"
-import {v4 as uuidv4} from "uuid"
-
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
     const result = await selectUsers()
-
     res.status(200).send(result)
-    
   } catch (error: any) {
     console.log(error)
     res.status(500).send(error.message || error.sqlMessage)
