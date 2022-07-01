489a20e7f9ecc02ada207d948ba5ffeef3edcd78
karlanatany <karlanatany3@gmail.com>
2022-07-01 14:26:44 -0300

    teste vida igual a zero

--- modulo7-bloco21/testes-e-mocks/tests/validateCharacter.test.ts
+++ modulo7-bloco21/testes-e-mocks/tests/validateCharacter.test.ts
@@ -10,4 +10,14 @@ describe(" Testando validateCharacter", () => {
     })
     expect(result).toBe(false)
   })
+
+  test("Deve retornar erro caso o objeto tenha a vida igual a zero", () => {
+    const result = validateCharacter({
+      name:"Personagem1",
+      life:0,
+      strength: 300,
+      defense: 500
+    })
+    expect(result).toBe(false)
+  })
 })
